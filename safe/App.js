522d7c48f5d546e0ffaa3576d433188a81596aaf
Surabhi Gupta <surabhi2029@gmail.com>
2023-02-18 20:41:56 -0800

    text to speech barebones

--- safe/App.js
+++ safe/App.js
@@ -1,43 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View, Button, TextInput } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import AppNavigator from './navigation/AppNavigator'
 import { NavigationContainer } from "@react-navigation/native";
-import FriendInvite from './pages/FriendInvite'
 
-// const goFriendPressed = () => {
-//   this.props.navigation.navigate("FriendInvite");
-// };
 export default function App() {
-  
-  return (
+	return (
+		<View style={styles.container}>
+			<NavigationContainer>
+				<AppNavigator />
+				{/* <HomeScreen/> */}
+			</NavigationContainer>
 
-    <View style={styles.container}>
-      <NavigationContainer>
-      <AppNavigator />
-      {/* <HomeScreen/> */}
-      </NavigationContainer>
-     
-      <StatusBar style="auto" />
-    </View>
-  );
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
-
-// const HomeScreen = ({navigation}) => {
-//   return (
-//     <Button
-//       title="Go to Jane's profile"
-//       onPress={() =>
-//         navigation.navigate('Profile', {name: 'Jane'})
-//       }
-//     />
-//   );
-// };
--- safe/pages/FriendInvite.js
+++ safe/pages/FriendInvite.js
@@ -1,14 +1,38 @@
-import React from "react";
-import { View, Image, Platform, Button, Text } from "react-native";
+import { View, Image, Platform, Button, Text, StyleSheet } from "react-native";
+import * as Speech from 'expo-speech';
+import React, { useState, useEffect } from 'react';
+import { Audio } from 'expo-av';
+
+const greeting = "my name is rishi sunak";
+const options = {
+	voice: "com.apple.speech.synthesis.voice.Fred",
+	pitch: 1.5,
+	rate: 0.7
+};
 
 class FriendInvite extends React.Component {
+
+	speakGreeting = () => {
+		Audio.setAudioModeAsync({ playsInSilentModeIOS: true })
+		Speech.speak(greeting, options)
+	};
+
 	render() {
 		return (
-			<View>
-				<Text>frferfgergfer</Text>
-
+			<View style={styles.container}>
+				<Button title="Speak" onPress={this.speakGreeting} />
 			</View>
 		);
 	}
+
+
 }
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
 export default FriendInvite;
